558dd8a9bdcba5c727fb275aa9bd837652c4461f
Danfei Xu <danfeix@nvidia.com>
2022-01-12 21:42:15 -0800

    tools for parameter search and launching experiments on ngc

--- ngc/launch_job.sh
+++ ngc/launch_job.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-NAME=$1
-RUNTIME=24h
+CONFIG_FILE=vae_latent1_c128_kl1e-1
+WANDB_PROJECT_NAME=vae-sweep
+
+RUNTIME=48h
 WS_ID=OVG6VSVJRjq7TTKBpOdohg # replace with your workspace ID
 WS_MOUNT_POINT=/workspace/ws_mount/
 DS_MOUNT_POINT=/workspace/lyft_prediction/
 RESULT_DIR=/workspace/result/
-CMD="cd $WS_MOUNT_POINT/tbsim; pip install -e .; pip install numpy==1.21.4;\
-python scripts/train_l5kit.py --output_dir $RESULT_DIR --name $NAME --config_name l5_raster_plan \
---dataset_path $DS_MOUNT_POINT/lyft_prediction --remove_exp_dir \
+
+# remember to set your WANDB_APIKEY!
+CMD="export WANDB_APIKEY=$WANDB_APIKEY; cd $WS_MOUNT_POINT/tbsim; pip install -e .; pip install numpy==1.21.4;\
+python scripts/train_l5kit.py --output_dir $RESULT_DIR --config_file experiments/danfei/$CONFIG_FILE.json \
+--name CONFIG_FILE --dataset_path $DS_MOUNT_POINT/lyft_prediction --remove_exp_dir --wandb_project_name $WANDB_PROJECT_NAME \
 & tensorboard --logdir $RESULT_DIR --bind_all"
+
+echo "$CMD"
 
 ngc batch run \
  --instance dgx1v.16g.1.norm \
- --name "$NAME" \
+ --name "$CONFIG_FILE" \
  --image "nvcr.io/nvidian/nvr-av/tbsim:latest" \
  --datasetid 90893:$DS_MOUNT_POINT \
  --workspace "$WS_ID":"$WS_MOUNT_POINT" \
