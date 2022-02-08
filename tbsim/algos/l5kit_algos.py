bug fixes: incorrect pixel_pred computation and goal indices

tbsim/algos/l5kit_algos.py
@@ -240,27 +240,13 @@ class SpatialPlanner(pl.LightningModule):
         ).squeeze(-1)
         residual_pred = local_pred[:, 1:3]
         yaw_pred = local_pred[:, 3:4]
-        return pixel_loc, residual_pred, yaw_pred, pixel_logit
-
-    def get_last_available_index(self, avails):
-        """
-        Args:
-            avails (torch.Tensor): target availabilities [B, (A), T]
-
-        Returns:
-            last_indices (torch.Tensor): index of the last available frame
-        """
-        num_frames = avails.shape[-1]
-        inds = torch.arange(0, num_frames).to(avails.device)  # [T]
-        inds = (avails > 0).float() * inds  # [B, (A), T] arange indices with unavailable indices set to 0
-        last_inds = inds.max(dim=-1)[1]  # [B, (A)] calculate the index of the last availale frame
-        return last_inds
+        return pixel_loc.float(), residual_pred, yaw_pred, pixel_logit
 
     def get_goal_supervision(self, data_batch):
         b, _, h, w = data_batch["image"].shape  # [B, C, H, W]
 
         # use last available step as goal location
-        goal_index = self.get_last_available_index(data_batch["target_availabilities"])[:, None, None]
+        goal_index = L5Utils.get_last_available_index(data_batch["target_availabilities"])[:, None, None]
 
         # gather by goal index
         goal_pos_agent = torch.gather(
@@ -306,12 +292,13 @@ class SpatialPlanner(pl.LightningModule):
         # decode map as predictions
         pixel_pred, res_pred, yaw_pred, pred_logit = self.decode_spatial_map(pred_map)
         # transform prediction to agent coordinate
-        pixel_pred = transform_points_tensor(
-            pixel_pred.unsqueeze(1).float(),
-            obs_dict["agent_from_raster"].float()
-        ).squeeze(1)
         pos_pred = transform_points_tensor(
             (pixel_pred + res_pred).unsqueeze(1),
+            obs_dict["agent_from_raster"].float()
+        ).squeeze(1)
+
+        pos_pred_no_res = transform_points_tensor(
+            pixel_pred.unsqueeze(1),
             obs_dict["agent_from_raster"].float()
         ).squeeze(1)
 
@@ -339,6 +326,8 @@ class SpatialPlanner(pl.LightningModule):
         goal_sup = data_batch["goal"]
         pos_norm_err = torch.norm(pred_batch["predictions"]["positions"] - goal_sup["goal_position"], dim=-1)
         metrics["goal_pos_err"] = torch.mean(pos_norm_err)
+        pos_norm_err_no_res = torch.norm(pred_batch["predictions_no_res"]["positions"] - goal_sup["goal_position"], dim=-1)
+        metrics["goal_pos_no_res_err"] = torch.mean(pos_norm_err_no_res)
         metrics["goal_yaw_err"] = torch.mean(torch.abs(pred_batch["predictions"]["yaws"] - goal_sup["goal_yaw"]))
 
         pixel_pred = torch.argmax(torch.flatten(pred_batch["spatial_prediction"][:, 0], start_dim=1), dim=1) # [B]

tbsim/models/l5kit_models.py
@@ -218,8 +218,13 @@ class RasterizedGCModel(RasterizedPlanningModel):
         image_batch = data_batch["image"]
         map_feat = self.map_encoder(image_batch)
         target_traj = torch.cat((data_batch["target_positions"], data_batch["target_yaws"]), dim=2)
-        goal_state = target_traj[:, -1]
-        goal_feat = self.goal_encoder(goal_state)
+        goal_inds = L5Utils.get_last_available_index(data_batch["target_availabilities"])
+        goal_state = torch.gather(
+            target_traj,  # [B, T, 3]
+            dim=1,
+            index=goal_inds[:, None, None].expand(-1, 1, target_traj.shape[-1])
+        ).squeeze(1)  # -> [B, 3]
+        goal_feat = self.goal_encoder(goal_state) # -> [B, D]
         input_feat = torch.cat((map_feat, goal_feat), dim=-1)
 
         if self.traj_decoder.dyn is not None:

tbsim/utils/loss_utils.py
@@ -238,9 +238,7 @@ def goal_reaching_loss(predictions, targets, availabilities, weights_scaling=Non
     """
     # compute loss mask by finding the last available target
     num_frames = availabilities.shape[-1]
-    inds = torch.arange(0, num_frames).to(targets.device)  # [T]
-    inds = (availabilities > 0).float() * inds  # [B, (A), T] arange indices with unavailable indices set to 0
-    last_inds = inds.max(dim=-1)[1]  # [B, (A)] calculate the index of the last availale frame
+    last_inds = L5Utils.get_last_available_index(availabilities)  # [B, (A)]
     goal_mask = TensorUtils.to_one_hot(last_inds, num_class=num_frames)  # [B, (A), T] with the last frame set to 1
     # filter out samples that do not have available frames
     available_samples_mask = availabilities.sum(-1) > 0  # [B, (A)]