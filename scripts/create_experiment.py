bbb413132079061b2094d7314bb518a4fc2a196f
Danfei Xu <danfeix@nvidia.com>
2022-04-25 18:07:18 -0700

    separate config search for nusc and l5kit

--- scripts/create_experiment.py
+++ scripts/create_experiment.py
@@ -4,69 +4,31 @@ import argparse
 from tbsim.utils.experiment_utils import create_configs, ParamSearchPlan, ParamRange, Param
 
 
-def configs_to_search(base_cfg):
+def configs_to_search_nusc(base_cfg):
     """Override this with your hyperparameter search plan"""
     plan = ParamSearchPlan()
-    # plan.add_const_param(Param("algo.dynamics.type", alias="dyn", value="Unicycle"))
-    #
-    # plan.extend(plan.compose_cartesian([
-    #     ParamRange("algo.optim_params.policy.learning_rate.initial", alias="lr", range=[1e-3, 1e-4]),
-    #     ParamRange("train.training.batch_size", alias="bs", range=[50, 100]),
-    # ]))
-    #
-    # plan.extend(plan.compose_zip([
-    #     ParamRange("algo.optim_params.policy.learning_rate.initial", alias="lr", range=[1e-3, 1e-4]),
-    #     ParamRange("algo.model_architecture", alias="arch", range=["resnet50", "resnet18"]),
-    # ]))
+    base_cfg.train.training.num_data_workers = 24
+    base_cfg.train.validation.num_data_workers = 8
 
-    # plan.extend(plan.compose_cartesian([
-    #     ParamRange("algo.dynamics.type", alias="dyn", range=[None, "Bicycle", "Unicycle"]),
-    #     ParamRange("algo.spatial_softmax.enabled", alias="ssmax", range=[False, True]),
-    #     ParamRange("algo.map_feature_dim", alias="mapfd", range=[64])
-    # ]))
-
-    # plan.extend(plan.compose_cartesian([
-    #     ParamRange("algo.spatial_softmax.enabled", alias="ssmax", range=[True]),
-    #     ParamRange("algo.spatial_softmax.kwargs.num_kp", alias="kp", range=[32, 64]),
-    #     ParamRange("algo.map_feature_dim", alias="mapfd", range=[64, 256]),
-    #     ParamRange("algo.decoder.layer_dims", alias="decmlp", range=[[], [128, 128]])
-    # ]))
-    # plan.extend(plan.compose_cartesian([
-    #     ParamRange("algo.spatial_softmax.enabled", alias="ssmax", range=[False]),
-    #     ParamRange("algo.decoder.layer_dims", alias="decmlp", range=[[], [128, 128]])
-    # ]))
-
-    # plan.extend(plan.compose_cartesian([
-    #     ParamRange("algo.dynamics.type", alias="dyn", range=["Bicycle", "Unicycle"]),
-    #     ParamRange("algo.loss_weights.collision_loss", alias="clw", range=[1.0]),
-    #     ParamRange("algo.loss_weights.goal_loss", alias="glw", range=[1.0]),
-    #     ParamRange("algo.loss_weights.prediction_loss", alias="plw", range=[0.0, 0.1])
-    # ]))
-
-    # plan.add_const_param(Param("train.rollout.enabled", alias="rl", value=False))
-    #
-    # plan.extend(plan.compose_zip([
-    #     ParamRange("algo.model_architecture", alias="arch", range=["resnet50", "resnet50"]),
-    #     ParamRange("train.training.batch_size", alias="bs", range=[64, 64]),
-    #     ParamRange("algo.loss_weights.pixel_ce_loss", alias="pcl", range=[0.0, 1.0]),
-    #     ParamRange("algo.loss_weights.pixel_bce_loss", alias="pbl", range=[1.0, 0.0]),
-    # ]))
-
-    # plan.extend(plan.compose_cartesian([
-    #     ParamRange("algo.dynamics.type", alias="dyn", range=[None, "Bicycle", "Unicycle"]),
-    #     ParamRange("algo.decoder.layer_dims", alias="decmlp", range=[[128, 128]])
-    # ]))
-
-    plan.add_const_param(
-        Param("train.rollout.enabled", alias="rl", value=False))
+    plan.add_const_param(Param("algo.history_num_frames", alias="ht", value=3))
+    plan.add_const_param(Param("algo.future_num_frames", alias="ft", value=10))
+    plan.add_const_param(Param("algo.step_time", alias="dt", value=0.5))
     plan.extend(plan.compose_cartesian([
-        ParamRange("algo.model_architecture",
-                   alias="arch", range=["resnet50"]),
-        ParamRange("algo.loss_weights.GAN_loss", alias="Gw", range=[0.1, 0.2]),
-        ParamRange("algo.use_transformer", alias="trans", range=[True, False]),
-        # ParamRange("algo.optim_params.policy.learning_rate.initial", alias="lr", range=[3e-4]),
-        ParamRange("algo.loss_weights.lane_reg_loss",
-                   alias="lreg", range=[0.5, 1.0]),
+        ParamRange("algo.loss_weights.yaw_reg_loss", alias="yrl", range=[0.01]),
+        ParamRange("algo.dynamics.type", alias="dyn", range=[None, "Unicycle"])
+    ]))
+
+    return plan.generate_configs(base_cfg=base_cfg)
+
+
+def configs_to_search_l5kit(base_cfg):
+    plan = ParamSearchPlan()
+    base_cfg.train.training.num_data_workers = 24
+    base_cfg.train.validation.num_data_workers = 8
+
+    plan.extend(plan.compose_zip([
+        ParamRange("algo.loss_weights.pixel_ce_loss", alias="clw", range=[0.0, 1.0]),
+        ParamRange("algo.loss_weights.pixel_bce_loss", alias="blw", range=[1.0, 0.0]),
     ]))
 
     return plan.generate_configs(base_cfg=base_cfg)
@@ -97,6 +59,13 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "--env",
+        type=str,
+        choices=["nusc", "l5kit"],
+        required=True
+    )
+
+    parser.add_argument(
         "--prefix",
         type=str,
         default=None,
@@ -104,12 +73,15 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    fn = configs_to_search_nusc if args.env == "nusc" else configs_to_search_l5kit
+    prefix = args.env
+    prefix += "_" + args.prefix if args.prefix is not None else ""
 
     create_configs(
-        configs_to_search,
+        fn,
         args.config_name,
         args.config_file,
         args.config_dir,
-        args.prefix,
+        prefix,
         delete_config_dir=False
     )
