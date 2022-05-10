448587268fe0bb306fa888d98000f64a2ae6376b
Danfei Xu <danfeix@nvidia.com>
2022-05-09 17:47:43 -0700

    prepare all baselines before eval

--- tbsim/evaluation/policy_composers.py
+++ tbsim/evaluation/policy_composers.py
@@ -218,6 +218,31 @@ class GAN(PolicyComposer):
         return policy, policy_cfg
 
 
+class GANplan(GAN):
+    def _get_predictor(self):
+        predictor_ckpt_path, predictor_config_path = get_checkpoint(
+            ngc_job_id=self.eval_config.ckpt.predictor.ngc_job_id,
+            ckpt_key=self.eval_config.ckpt.predictor.ckpt_key,
+            ckpt_root_dir=self.ckpt_root_dir
+        )
+        predictor_cfg = get_experiment_config_from_file(predictor_config_path)
+
+        predictor = MATrafficModel.load_from_checkpoint(
+            predictor_ckpt_path,
+            algo_config=predictor_cfg.algo,
+            modality_shapes=self.get_modality_shapes(predictor_cfg),
+        ).to(self.device).eval()
+        return predictor, predictor_cfg.clone()
+
+    def get_policy(self, policy=None):
+        policy, policy_cfg = super(GANplan, self).get_policy(policy=policy)
+        predictor, _ = self._get_predictor()
+
+        policy = SamplingPolicyWrapper(ego_action_sampler=policy, agent_traj_predictor=predictor)
+        policy = PolicyWrapper.wrap_controller(policy, cost_weights=self.eval_config.policy.cost_weights)
+        return policy, policy_cfg
+
+
 class Hierarchical(PolicyComposer):
     def _get_planner(self):
         planner_ckpt_path, planner_config_path = get_checkpoint(
@@ -272,6 +297,29 @@ class Hierarchical(PolicyComposer):
             planner,
             mask_drivable=self.eval_config.policy.mask_drivable,
             sample=False
+        )
+        policy = HierarchicalWrapper(planner, controller)
+        return policy, exp_cfg
+
+
+class HierarchicalSample(Hierarchical):
+    def get_policy(self, planner=None, controller=None):
+        if planner is not None:
+            assert isinstance(planner, SpatialPlanner)
+        else:
+            planner, exp_cfg = self._get_planner()
+
+        if controller is not None:
+            assert isinstance(controller, MATrafficModel)
+            exp_cfg = None
+        else:
+            controller, exp_cfg = self._get_controller()
+            exp_cfg = exp_cfg.clone()
+
+        planner = PolicyWrapper.wrap_planner(
+            planner,
+            mask_drivable=self.eval_config.policy.mask_drivable,
+            sample=True
         )
         policy = HierarchicalWrapper(planner, controller)
         return policy, exp_cfg
