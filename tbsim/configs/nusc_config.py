5841d752b419f4f3518ad0f4cb51b864c831e9bf
rkcosner <rkcosner@caltech.edu>
2022-08-03 11:02:15 -0700

    fixing gamma plotting flips

--- tbsim/configs/nusc_config.py
+++ tbsim/configs/nusc_config.py
@@ -32,7 +32,7 @@ class NuscTrainConfig(TrainConfig):
         self.validation.enabled = True
         self.validation.batch_size = 5
         self.validation.num_data_workers = 6
-        self.validation.every_n_steps = 500
+        self.validation.every_n_steps = 200
         self.validation.num_steps_per_epoch = 10
 
 
